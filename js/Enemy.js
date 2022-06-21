a99a5b13ad1534dd830113e4c86f64216f1e932c
WashingtonYandun <washington.yandun06@gmail.com>
2022-06-20 23:01:27 -0500

    add some initila classes for the project

---
+++ js/Enemy.js
@@ -0,0 +1,13 @@
+class Enemy {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+import { Player } from "./Player.js";
+import { Enemy } from "./Enemy.js";
+import { Bullet } from "./Bullet.js";
+import { Score } from "./Score.js";
+import { Game } from "./Game.js";
+import { GameOver } from "./GameOver.js";
+import { GameWin } from "./GameWin.js";
+import { GamePause } from "./GamePause.js";
+import { GameStart } from "./GameStart.js";
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
