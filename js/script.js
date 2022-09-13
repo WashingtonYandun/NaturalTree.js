fb353212ab1d838ad9815c239e17fb2b36a69e52
Washington Yandun <washington.yandun06@gmail.com>
2022-09-12 23:23:44 -0500

    add a the core of the script (not funtional yet)

--- js/script.js
+++ js/script.js
@@ -38,5 +38,55 @@ const drawLine = (x1, y1, x2, y2, color, width) => {
     ctx.lineWidth = width;
     ctx.stroke();
     ctx.closePath();
-    ``;
 };
+
+const generate = () => {
+    let nextStringSystem = "";
+    for (let i = 0; i < stringSystem.length; i++) {
+        let current = stringSystem[i];
+        let found = false;
+        for (let j = 0; j < rules.length; j++) {
+            if (current == rules[j].a) {
+                found = true;
+                nextStringSystem += rules[j].b;
+                break;
+            }
+        }
+        if (!found) {
+            nextStringSystem += current;
+        }
+    }
+    stringSystem = nextStringSystem;
+    console.log(stringSystem);
+};
+
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.strokeColor = "white";
+    ctx.translate(canvas.width / 2, canvas.height);
+    ctx.moveTo(0, 0);
+    for (let i = 0; i < stringSystem.length; i++) {
+        let current = stringSystem[i];
+        if (current === "F") {
+            ctx.lineTo(0, -20);
+            ctx.translate(0, -20);
+        } else if (current === "+") {
+            ctx.rotate((Math.PI * 25) / 180);
+        } else if (current === "-") {
+            ctx.rotate(-(Math.PI * 25) / 180);
+        } else if (current === "[") {
+            ctx.save();
+        } else if (current === "]") {
+            ctx.restore();
+        }
+    }
+    ctx.stroke();
+};
+
+const main = () => {
+    generate();
+    draw();
+};
+
+main();
