a5588234b387dbadaead93b3597f3a4ea3b5e64a
Washington Yandun <washington.yandun06@gmail.com>
2022-09-13 23:10:48 -0500

    delete duplicated function and finally draw something

--- js/script.js
+++ js/script.js
@@ -10,26 +10,6 @@ let rules = [
     },
 ];
 
-const generate = () => {
-    let nextStringSystem = "";
-    for (let i = 0; i < stringSystem.length; i++) {
-        let current = stringSystem[i];
-        let found = false;
-        for (let j = 0; j < rules.length; j++) {
-            if (current == rules[j].a) {
-                found = true;
-                nextStringSystem += rules[j].b;
-                break;
-            }
-        }
-        if (!found) {
-            nextStringSystem += current;
-        }
-    }
-    stringSystem = nextStringSystem;
-    console.log(stringSystem);
-};
-
 const drawLine = (x1, y1, x2, y2, color, width) => {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -69,8 +49,8 @@ const draw = () => {
     for (let i = 0; i < stringSystem.length; i++) {
         let current = stringSystem[i];
         if (current === "F") {
-            ctx.lineTo(0, -20);
-            ctx.translate(0, -20);
+            drawLine(0, 0, 0, -10, "white", 1);
+            ctx.translate(0, -10);
         } else if (current === "+") {
             ctx.rotate((Math.PI * 25) / 180);
         } else if (current === "-") {
